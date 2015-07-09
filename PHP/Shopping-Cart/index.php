d9b0f38881c622f668279be9a6048fddb9ce8ffe
jmyroup <j.myroup@yahoo.com>
2015-07-09 13:33:11 -0700

    Create index.php

---
+++ PHP/Shopping-Cart/index.php
@@ -0,0 +1,9 @@
+<?php
+$page_title = "Let's go shopping!";
+include ('header.html');
+?>
+<p align="center"><b>Getting ready to go back to school?</b></p>
+<p align="center"><a href="products.php">Let's go shopping!</a></p>
+<?php
+include('footer.html');
+?>
