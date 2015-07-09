8aabe6a7363c0a7a9010f157dcec238cb5b57ace
jmyroup <j.myroup@yahoo.com>
2015-07-09 13:48:06 -0700

    Create header.php

---
+++ PHP-MySQL/CMS/templates/include/header.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title><?php echo htmlspecialchars($results['pageTitle'])?></title>
+    <link rel="stylesheet" type="text/css" href="style.css" />
+  </head>
+  <body>
+    <div id="container">
+ 
+      <a href="."><img id="logo" src="https://cloud.githubusercontent.com/assets/13230007/8606462/95c573de-2640-11e5-964f-8e3b982dc571.jpg" alt="Widget News" /></a>
