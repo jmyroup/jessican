3fe7a4eb069b1fe29573958c014c5fc54a467b41
Jessica Myroup <j.myroup@yahoo.com>
2015-07-10 13:20:01 -0700

    Create 3.php

---
+++ PHP/user-auth/3.php
@@ -0,0 +1,11 @@
+<?php
+require 'auth.php';
+
+
+    print "Welcome, " . $username;
+	print '<br /><br />';
+	print '<a href="1.php">page one</a> | ';
+	print '<a href="2.php">page two</a> | ';
+	print '<a href="logout.php">logout</a>';
+	
+?>
