5d862d8331349a1e8afb3df140676a7b928bd7e1
Jessica Myroup <j.myroup@yahoo.com>
2015-07-10 12:30:53 -0700

    Create hitCounter.php

---
+++ PHP-MySQL/hitCounter.php
@@ -0,0 +1,30 @@
+<?php
+$dbHost = "127.0.0.1";
+$dbUser = "jess";
+$dbPass = "password";
+$dbDatabase = "guestbook";
+
+// Connect to DB
+
+$db = mysqli_connect($dbHost, $dbUser, $dbPass, $dbDatabase);
+
+if ($db ->connect_errno > 0){
+	die('Could not select DB');
+}
+$db->select_db("hitcounter");
+$update_query = "update hitcounter set hits = hits + 1";
+
+if (!$results = $db->query($update_query)){
+	die('Error running query ' . $db->error );
+}
+
+$counter_query = "select hits from hitcounter";
+$get_rs = $db->query($counter_query);
+
+while ($row = $get_rs->fetch_assoc()){
+	echo "Number of visits: " . $row["hits"];
+}
+
+$get_rs->free();
+$db->close();
+?>
