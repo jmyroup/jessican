151efe65872b532bd45644396dd91826d21ede26
Jessica Myroup <j.myroup@yahoo.com>
2015-08-07 17:03:48 -0700

    Improved security; added hitcounter

--- PHP-MySQL/guestbook_script.php
+++ PHP-MySQL/guestbook_script.php
@@ -11,34 +11,71 @@ $li = mysqli_connect($dbHost, $dbUser, $dbPass, $dbDatabase) or die("Could not c
 $gb_str = ""; 	// $gb_str is the string we'll append entries to
 $pgeTitle = "View and Sign Guestbook";
 
-// If form is submitted, then insert into DB
-if (!empty($_POST["submit"])) {
-	$name = $_POST["gbName"];
-	$email = $_POST["gbEmail"];
-	$comment = $_POST["gbComment"];
-	$date = Date("Y-m-d h:i:s");
+// If form is submitted, validate the data
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+	$_POST["gbName"] = htmlentities($_POST["gbName"]);
+	$_POST["gbEmail"] = htmlentities($_POST["gbEmail"]);
+	$_POST["gbComment"] = htmlentities($_POST["gbComment"]);
 	
-	$gb_query = "insert into guestbook (gbName, gbEmail, gbComment, gbDateAdded) values ('$name', '$email', '$comment', '$date')";
+	// spam purification
+	function spam_scrubber($value) {
+		
+		// List of very bad values
+		$very_bad = array("to:", "cc:", "bcc:", "content-type:", "mime-version:", "multipart-mixed:", "content-transfer-encoding:");
+		// If present, return an empty string
+		foreach ($very_bad as $v) {
+			if (stripos($value, $v) !== false) return "";
+		}
+		
+		// Replace any newline characters with spaces
+		$value = str_replace(array( "\r", "\n", "%0a", "%0d"), " ", $value);
+		
+		// Return the value
+		return trim($value);
+	} // end of spam_scrubber() function
 	
-	mysqli_query($li, $gb_query);
-	$res = mysqli_affected_rows($li);
+	// clean the form data
+	$scrubbed = array_map("spam_scrubber", $_POST);
+
+	// Handle the form
+	if (!empty($scrubbed["gbComment"])) {
 	
-	// See if insert was successful or not
-	if($res > 0) {
-		$ret_str="Your guestbook entry was successfully added.";
-	} else {
-		$ret_str = "Your guestbook entry was NOT successfully added.";
+		// Create the query
+		$gb_query = "INSERT INTO guestbook (gbName, gbEmail, gbComment, gbDateAdded) VALUES (?, ?, ?, NOW())";
+		
+		// Prepare the statement
+		$stmt = mysqli_prepare($li, $gb_query);
+		
+		// Bind the variables:
+		mysqli_stmt_bind_param($stmt, 'sss', $name, $email, $comment);
+		
+		// Assign values to the variables
+		$name = $scrubbed["gbName"];
+		$email = $scrubbed["gbEmail"];
+		$comment = $scrubbed["gbComment"];
+		
+		// Execute the query
+		mysqli_stmt_execute($stmt);		
+	
+		$res = mysqli_stmt_affected_rows($stmt);
+	
+		// See if insert was successful or not
+		if($res == 1) {
+			$ret_str = "Your guestbook entry was successfully added.";
+		} else {
+			$ret_str = "Your guestbook entry was NOT successfully added.";
+		}
+	
+		// Append success/failure message
+		$gb_str .= "<span class=\"ret\">$ret_str</span><br />";
 	}
-	
-	// Append success/failure message
-	$gb_str .= "<span class=\"ret\">$ret_str</span><BR>";
-	echo mysqli_error($li);
 }
-// The querystring
-$get_query = "select gbName, gbEmail, gbComment, DATE_FORMAT(gbDateAdded, '%m-%d-%y %H:%i') gbDateAdded
-		from guestbook";
+
+// Get entries from database
+$get_query = "SELECT gbName, gbEmail, gbComment, DATE_FORMAT(gbDateAdded, '%m-%d-%y %H:%i') gbDateAdded
+		FROM guestbook";
 $get_rs = mysqli_query($li, $get_query);
-$gb_str .= "<hr size=\"1\">";
+$gb_str .= "<hr size=\"1\" />";
 
 // While there are still results
 while($get_row = mysqli_fetch_array($get_rs)) {
@@ -57,11 +94,11 @@ while($get_row = mysqli_fetch_array($get_rs)) {
 		$name = "<a href=\"mailto:$email\">$email</a>";
 	// Else make name Anonymous 
 	} else {
-		$name = "by Anonymous";
+		$name = "Anonymous";
 	}
 	
 	// Append to string we'll print later on
-	$gb_str .= "<br>$comment<p class=\"small\">< posted on $date by $name><hr size=\"1\">";
+	$gb_str .= "<br />$comment<p class=\"small\">< posted on $date by $name ></p><hr size=\"1\" />";
 }
 
 // Hitcounter
@@ -73,70 +110,49 @@ $hits = $rows['hits'];
 // Free Result Memory
 mysqli_free_result($get_rs);
 mysqli_free_result($query);
+
+// Close MySQL Connection
+mysqli_stmt_close($stmt);
+mysqli_close($li);
 ?>
 
 <!doctype html>
 <html>
 <head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href='http://fonts.googleapis.com/css?family=Josefin+Slab|Libre+Baskerville|Port+Lligat+Slab' rel='stylesheet' type='text/css' />
 	<title>Guestbook</title>
-	<script language="javascript">
-	/* This function is pulled from a generic validation file from
-	some other site (probably developer.netscape.com) and strips out
-	characters you don't want */
-	function stripCharsInBag (s, bag) {
-		var i;
-    	var returnString = "";
-    	// Search through string's characters one by one.
-    	// If character is not in bag, append to returnString.
-    	for (i = 0; i < s.length; i++)
-    	{   
-        	// Check that current character isn't whitespace.
-        	var c = s.charAt(i);
-        	if (bag.indexOf(c) == -1) returnString += c;
-    	}
-    	return returnString;
-	}
-	// This function just makes sure the comment field is not empty
-	function valForm(frm) {
-		badChars = "<[]>{}";
-		if(frm.gbComment.value == "") {
-			alert("Please fill in your comments for the guestbook.");
-			return false;
-		} else {
-			frm.gbComment.value = stripCharsInBag(frm.gbComment.value, badChars);
-			// These values may be empty, but strip chars in case they're not
-			frm.gbName.value = stripCharsInBag(frm.gbName.value, badChars);
-			frm.gbEmail.value = stripCharsInBag(frm.gbEmail.value, badChars);
-			return true;
-		}
-	}
-	</script>
 </head>
-<body bgcolor="#FFFFFF"><?php echo $gb_str; ?><form name="gb" action="<?php echo $_SERVER['PHP_SELF'];?>" method="post">		
-	<table cellpadding="3" cellspacing="0" border="0">
-    	<tr>
-        	<td class="tdhead" valign="top" align="right">Name</td>
-            <td valign="top"><input type="text" name="gbName" value="" size="30" maxlength="50"></td>
-        </tr>
-        <tr>
-        	<td class="tdhead" valign="top" align="right">Email</td>
-            <td valign="top"><input type="text" name="gbEmail" value="" size="30" maxlength="100"></td>
-        </tr>
-        <tr>
-        	<td class="tdhead" valign="top" align="right">Comment</td>
-            <td valign="top"><textarea name="gbComment" rows="5" cols="30"></textarea></td>
-        </tr>
-        <tr>
-        	<td></td>
-            <td><input type="submit" name="submit" value="submit" onClick="return valForm(document.gb)"><input type="reset" name="reset" value="reset"></td>
-        </tr>
-    </table></form>
-	<br />
+<body bgcolor="#FFFFFF">
+	<h1>Please feel free to leave feedback!</h1>
+	<form name="gb" action="guestbook.php" method="post">		
+		<table cellpadding="3" cellspacing="0" border="0">
+    		<tr>
+        		<td class="tdhead" valign="top" align="right">Name</td>
+            	<td valign="top"><input type="text" name="gbName" value="" size="30" maxlength="50" /></td>
+        	</tr>
+        	<tr>
+        		<td class="tdhead" valign="top" align="right">Email</td>
+            	<td valign="top"><input type="text" name="gbEmail" value="" size="30" maxlength="100" /></td>
+        	</tr>
+        	<tr>
+        		<td class="tdhead" valign="top" align="right">Comment</td>
+            	<td valign="top"><textarea name="gbComment" rows="5" cols="30" required></textarea></td>
+        	</tr>
+        	<tr>
+        		<td></td>
+            	<td><input type="submit" name="submit" value="submit" />
+                <input type="reset" name="reset" value="reset" /></td>
+        	</tr>
+    	</table>
+    </form>
+    <br />
+	<?php echo $gb_str; ?>
+    <br />
+    <br />
 	Visitors: <?php echo $hits; ?>
+    <br /><br />
+    <a href="../../../projects.php">Click here to return to the projects page</a>
 </body>
 </html>
-
-<?php
-// Close MySQL Connection
-mysqli_close($li);
-?>
